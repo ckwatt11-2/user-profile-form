4bb3bcf889198ef3985854d589c7c016336363b7
unknown <pawtee80085@gmail.com>
2025-05-28 07:23:36 -0700

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            userNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Database connected!")
+    }
+    catch (err){
+        console.log(`Error: ${err.message}`);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
